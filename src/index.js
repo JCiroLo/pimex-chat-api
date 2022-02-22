bd0dfb8543451827abb19516682147ffa7d5f857
JCiroLo <juanciro35@gmail.com>
2022-02-22 12:02:22 -0500

    Socket implemetation initialized

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 const express = require('express')
 const cors = require('cors')
-const app = express()
-// const morgan = require('morgan')
+const http = require('http')
+const morgan = require('morgan')
+const socketio = require('socket.io')
 const { validate } = require('./lib/utils')
 
-app.use(cors())
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server, {
+  cors: {
+    origin: '*'
+  }
+})
 
+io.on('connection', socket => {
+  console.log('New websocket connetion')
+
+  socket.emit('message', 'Welcome to Chat')
+})
+
+app.use(cors())
 app.use(express.json())
-// app.use(morgan('dev'))
+app.use(morgan('dev'))
 // app.use(validate)
 
 // settings
@@ -24,5 +38,6 @@ app.use('/messages', require('./routes/messages'))
 app.use('/users', require('./routes/users'))
 app.use('/leads', require('./routes/leads'))
 
-app.listen(app.get('port'))
-console.log('Server on port', app.get('port'))
+server.listen(app.get('port'), () => {
+  console.log('Server on port', app.get('port'))
+})
